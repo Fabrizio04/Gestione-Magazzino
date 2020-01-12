0821b846e9ead8a3b0a73da93b6bacde055407da
Fabrizio04 <amorellif94@gmail.com>
2020-01-12 17:53:43 +0100

    PHP Script

--- scarico/index.php
+++ scarico/index.php
@@ -213,7 +213,7 @@ while($d = $q->fetch_array()){
 
 <br>
 
-<h3 style="text-align:center;"><img src="./AntiCopy.png" width="16" height="16"> 2018 Gestione Magazzino by Fabrizio Amorelli e Giulia Sedda</h3>
+<h3 style="text-align:center;"><img src="./AntiCopy.png" width="16" height="16"> 2018 Gestione Magazzino by Fabrizio Amorelli</h3>
 
 </center>
 
